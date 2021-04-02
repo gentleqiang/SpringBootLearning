97dde6cfa839d73af3a8fb9450967c6e25e98349
gentleqiang <gentleman_qiang@163.com>
2021-04-03 07:52:23 +0800

    APISIGN

---
+++ server/src/main/java/com/gentleman/server/ApiSign/RedisConfiguration.java
@@ -0,0 +1,13 @@
+package com.gentleman.server.ApiSign;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 一粒尘埃
+ * @date 2021/4/3/7:51
+ */
+@Configuration
+public class RedisConfiguration {
+
+
+}
